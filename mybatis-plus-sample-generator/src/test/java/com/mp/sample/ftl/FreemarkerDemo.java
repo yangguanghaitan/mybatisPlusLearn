41161bb0447e5896be643e19c52e8ba09cae1131
yangguanghaitan <2898666969@qq.com>
2022-01-29 14:56:35 +0800

    mybatis-plus-sample-generator+freemarker简单使用+2

---
+++ mybatis-plus-sample-generator/src/test/java/com/mp/sample/ftl/FreemarkerDemo.java
@@ -0,0 +1,44 @@
+package com.mp.sample.ftl;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther d
+ * @Date 2022/1/29 1:41
+ * @Describe 测试Freemarker
+ **/
+public class FreemarkerDemo {
+    public static void main(String[] args) throws IOException, TemplateException {
+        final Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File("D:\\AAAA\\bbb\\templates"));
+        cfg.setDefaultEncoding("UTF-8");
+        //设置错误的如何表现
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);//
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
+
+        //数据模型
+        Map<String, Object> root = new HashMap<>();
+        root.put("user", "Big Joe");
+        Product latest = new Product();
+        latest.setUrl("products/greenmouse.html");
+        latest.setName("green mouse");
+        root.put("latestProduct", latest);
+
+        //模板
+        Template temp = cfg.getTemplate("test.ftlh");
+        Writer out = new OutputStreamWriter(System.out);
+        temp.process(root, out);
+    }
+}
---
+++ mybatis-plus-sample-generator/src/test/java/com/mp/sample/ftl/Product.java
@@ -0,0 +1,27 @@
+package com.mp.sample.ftl;
+
+/**
+ * @Auther d
+ * @Date 2022/1/29 1:55
+ * @Describe
+ **/
+public class Product {
+    private String url;
+    private String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
